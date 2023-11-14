e59d1d3d0045219d0d01e764541b62c0aa227d63
zhangyang <zhangyang@wisecotech.com>
2023-11-14 12:02:56 +0800

    code generator create

---
+++ springboot-code-generator/src/main/java/com/appril/CodeApplication.java
@@ -0,0 +1,12 @@
+package com.appril;
+
+
+/**
+ * @author zhangyang
+ * @date 2023/11/14 11:22
+ */
+public class CodeApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
